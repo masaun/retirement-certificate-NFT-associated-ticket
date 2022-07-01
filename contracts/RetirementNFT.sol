f0143241bc994231864becc7aab896c630b848de
masaun <masanoriuno@gmail.com>
2022-07-01 17:08:50 +0900

    [SC]: Import ERC721, Ownable into RetirementNFTBundledTicket.sol and RetirementNFT.sol

--- contracts/RetirementNFT.sol
+++ contracts/RetirementNFT.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
 
 /**
  * @title The Retirement NFT contract
  */
-contract RetirementNFT {
+contract RetirementNFT is ERC721, Ownable {
 
-    constructor() {
+    constructor() ERC721("Retirement NFT", "RNFT") {
         //[TODO]: 
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
     }
 
 }
--- contracts/RetirementNFTBundledTicket.sol
+++ contracts/RetirementNFTBundledTicket.sol
@@ -5,16 +5,23 @@ pragma solidity ^0.8.7;
 import { RandomNumberConsumerV2 } from "./chainlink-examples/RandomNumberConsumerV2.sol";
 
 //@dev - Retirement NFT
-import { RetirementNFT } from "RetirementNFT.sol";
+import { RetirementNFT } from "./RetirementNFT.sol";
 
+//@dev - NFT
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title The Retirement NFT bundled Ticket contract
  */
-contract RetirementNFTBundledTicket {
+contract RetirementNFTBundledTicket is ERC721, Ownable {
 
-    constructor() {
+    constructor() ERC721("Retirement NFT bundled Ticket", "RNFT_BUNDLED_TICKET") {
         //[TODO]: 
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
     }
 
 }
