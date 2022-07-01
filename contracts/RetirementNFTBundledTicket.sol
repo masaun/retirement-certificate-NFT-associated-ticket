7fec1a31b4e4ca459d9c30399b4016af081be62b
masaun <masanoriuno@gmail.com>
2022-07-01 16:44:46 +0900

    [SC]: Define the RetirementNFTBundledTicket.sol

---
+++ contracts/RetirementNFTBundledTicket.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+/**
+ * @title The Retirement NFT bundled Ticket contract
+ */
+contract RetirementNFTBundledTicket {
+
+    constructor() {
+        //[TODO]: 
+    }
+
+}
