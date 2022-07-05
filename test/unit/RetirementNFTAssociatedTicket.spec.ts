91a27bd7c8a378d19aef03b05aa023fa7744f896
masaun <masanoriuno@gmail.com>
2022-07-05 19:42:20 +0900

    [Test]: Assign arg values into mintRetirementNFTAssociatedTicket() of the RetirementNFTAssociatedTicketFactory.sol  in the RetirementNFTAssociatedTicket.spec.ts

--- test/unit/RetirementNFTAssociatedTicket.spec.ts
+++ test/unit/RetirementNFTAssociatedTicket.spec.ts
@@ -3,7 +3,7 @@ import { assert, expect } from "chai"
 import { BigNumber, ContractReceipt, ContractTransaction } from "ethers"
 import { network, deployments, ethers, run } from "hardhat"
 import { developmentChains } from "../../helper-hardhat-config"
-import { RetirementNFTAssociatedTicket, LinkToken, MockOracle } from "../../typechain"
+import { RetirementNFTAssociatedTicket, RetirementNFTAssociatedTicketFactory, LinkToken, MockOracle } from "../../typechain"
 
 
 /**
@@ -13,15 +13,29 @@ import { RetirementNFTAssociatedTicket, LinkToken, MockOracle } from "../../type
     ? describe.skip
     : describe("RetirementNFTAssociatedTicket Unit Tests", async function () {
           let retirementNFTAssociatedTicket: RetirementNFTAssociatedTicket
+          let retirementNFTAssociatedTicketFactory: RetirementNFTAssociatedTicketFactory
           let linkToken: LinkToken
           let mockOracle: MockOracle
 
           beforeEach(async () => {
               await deployments.fixture(["mocks", "api"])
+
               linkToken = await ethers.getContract("LinkToken")
               const linkTokenAddress: string = linkToken.address
-              retirementNFTAssociatedTicket = await ethers.getContract("RetirementNFTAssociatedTicketFactory")
+
               mockOracle = await ethers.getContract("MockOracle")
+
+              //@dev - Create the contract instance of the RetirementNFTAssociatedTicketFactory.sol
+              retirementNFTAssociatedTicketFactory = await ethers.getContract("RetirementNFTAssociatedTicketFactory")
+
+              //@dev - A new RetirementNFTAssociatedTicket is minted
+              const to = "0xb794F5eA0ba39494cE839613fffBA74279579268"
+              const ticketType = 0 
+              const mintAmount = 100  // Number of tickets to be minted (ERC1155)
+              const retirementNFT = "0x343c43A37D37dfF08AE8C4A11544c718AbB4fCF8"
+              const uri = "https://gateway.pinata.cloud/ipfs/QmPK1s3pNYLi9ERiq3BDxKa4XosgWwFRQUydHUtz4YgpqB"
+              let tx = await retirementNFTAssociatedTicketFactory.mintRetirementNFTAssociatedTicket(to, ticketType, mintAmount, retirementNFT, uri)
+              let txReceipt = await tx.wait()
 
               await run("fund-link", { contract: retirementNFTAssociatedTicket.address, linkaddress: linkTokenAddress })
           })
