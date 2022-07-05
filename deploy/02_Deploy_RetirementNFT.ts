74a416ad726ca2baf5723736c045dd976e729ee3
masaun <masanoriuno@gmail.com>
2022-07-05 12:38:04 +0900

    [Deploy]: Define the 02_Deploy_RetirementNFT.ts  under the ./deploy

---
+++ deploy/02_Deploy_RetirementNFT.ts
@@ -0,0 +1,43 @@
+import { DeployFunction } from "hardhat-deploy/types"
+import { network, ethers, run } from "hardhat"
+import {
+  networkConfig,
+  developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+} from "../helper-hardhat-config"
+import { autoFundCheck, verify } from "../helper-functions"
+
+const deployFunction: DeployFunction = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log, get } = deployments
+
+  const { deployer } = await getNamedAccounts()
+  const chainId: number | undefined = network.config.chainId
+  if (!chainId) return
+
+  const waitBlockConfirmations: number = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS
+
+  //@dev - Deploy the RetirementNFT contract
+  const args: any = []  // [NOTE]: Argument values for constructor
+  const retirementNFT = await deploy(`RetirementNFT`, {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  })
+
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...")
+    await verify(retirementNFT.address, args)
+  }
+
+  
+  log(`Run RetirementNFT contract with following command:`)
+  const networkName: string = network.name == "hardhat" ? "localhost" : network.name
+  log(`yarn hardhat request-data --contract ${retirementNFT .address} --network ${networkName}`)
+  log(`----------------------------------------------------`)
+}
+
+export default deployFunction
+deployFunction.tags = [`all`, `api`, `main`]
